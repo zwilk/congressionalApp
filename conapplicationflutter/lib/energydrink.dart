4979fe091839545b1f5e210e423c559c15537db0
NataliaFeinberg <nf0001@students.inghamisd.org>
2025-10-07 09:29:10 -0400

    push1
    you can add drinks! still working on the naming part tho sinc eit doesnt seem to like inputs

---
+++ conapplicationflutter/lib/energydrink.dart
@@ -0,0 +1,40 @@
+class Energydrink {
+
+  int howMany = 0;
+  String flavorName = " ";
+  String brandName = " ";
+  int IDNumber = 0;
+
+  Energydrink(String flavor, String brand, int ID){
+
+    flavorName = flavor;
+    brandName = brand;
+  }
+
+  int drink (){
+    howMany = howMany + 1;
+    return howMany;
+  }
+
+  String getBrand(){
+  return brandName;
+  }
+
+  String getName(){
+  return flavorName;
+  }
+
+  int getID(){
+    return IDNumber;
+  }
+
+  void printDrinkProfile(){
+    print("_____________________________________________________________");
+    print(flavorName);
+    print(brandName);
+    print("you have drank: $howMany cans");
+    print("_____________________________________________________________");
+  }
+  
+
+}  
